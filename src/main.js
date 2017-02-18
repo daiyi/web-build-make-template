a5863b17f08095c7e026915a1f4614bf0fc3537d
Jeremy Morrell <morrell.jeremy@gmail.com>
2017-02-17 18:48:20 -0800

    Tweaks

--- src/main.js
+++ src/main.js
@@ -1,4 +1,1 @@
-window.addEventListener('load', () => {
-    document.querySelector('h1').append(['hello', 'potato'].reduce((str, word) => str += `${word}! `, ''))
-  }
-);
+document.querySelector('h1').append(['hello', 'potato'].reduce((str, word) => str += `${word}! `, ''))
